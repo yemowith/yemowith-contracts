59aed96fe871aa4feda723da0366172ca9411866
yemowith <yemowith@gmail.com>
2024-05-21 13:58:11 +0300

    update

--- contracts/Scripts/Factories/SimpleFactory.sol
+++ contracts/Scripts/Factories/SimpleFactory.sol
@@ -1,4 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SimpleFactory {}
+contract SimpleFactory {
+    struct ContractForFactory {}
+    address[] public contracts;
+
+    function createContract(address _contract) public {
+        contracts.push(_contract);
+    }
+
+    function getContracts() public view returns (address[] memory) {
+        return contracts;
+    }
+
+    function getContract(uint256 index) public view returns (address) {
+        return contracts[index];
+    }
+}
