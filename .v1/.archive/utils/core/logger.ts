a28c7da978626c51294d616afd54efe088f482d5
yemowith <yemowith@gmail.com>
2024-05-24 06:48:57 +0300

    v2-dev

--- .v1/.archive/utils/core/logger.ts
+++
@@ -1,9 +0,0 @@
-import { consola, createConsola } from "consola";
-
-const logg = consola;
-
-const logBalance = (balance: number) => {
-    logg.info(`Balance: ${balance}`);
-}
-
-export  {logg,logBalance};
