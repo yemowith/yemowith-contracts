66ce37a892b12143376ee20f8a55d0638e8f3eff
yemowith <yemowith@gmail.com>
2024-05-24 06:19:58 +0300

    update

--- src/js/global.d.ts
+++
@@ -1,7 +0,0 @@
-import _spc from "../../.archive/utils/_base";
-
-type SPCType = typeof _spc;
-
-declare global {
-  var _spc: SPCType;
-}
--- src/js/scripts/localnode/1_init_space.ts
+++
@@ -1,3 +0,0 @@
-const action = async () => {};
-
-export default action;
